b2ae14a253feb76b5c19d8e1565d1768fa28fa0d
Connor Lagana <connor.lagana@gmail.com>
2020-01-16 10:27:52 -0500

    🥑

---
+++ ReactFrontEnd/client/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Router, Link } from "react-router-dom";
+
+function Login() {
+  return (
+    <div id="login">
+      <div id="backframeLogin">Hola</div>
+    </div>
+  );
+}
+
+export default Login;
