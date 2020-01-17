7f198eae3016d7c29a61a0cddd2dffdb22fa1d5a
Connor Lagana <connor.lagana@gmail.com>
2020-01-17 13:02:06 -0500

    🍭

--- ReactFrontEnd/client/src/components/Profile/Profile.js
+++ ReactFrontEnd/client/src/components/Profile/Profile.js
@@ -1,21 +1,37 @@
 import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
+import ProfileHeader from "./ProfileHeader.js";
+import axios from "axios";
 
 class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
       name: "",
-      image_url: ""
+      image_url: "",
+      description: "ff"
     };
   }
+
+  async componentDidMount() {
+    const res = await axios.get("http://localhost:3001/users/2");
+    console.log(res);
+
+    this.setState({
+      name: res.data.username,
+      image_url: res.data.image_url,
+      description: res.data.description
+    });
+  }
+
   render() {
     return (
-      <div>
-        <h1>Welcome {this.state.name}</h1>
-        <div>
-          <img id="profilePicture" src={this.state.image_url} />
-        </div>
+      <div id="profile">
+        <ProfileHeader
+          name={this.state.name}
+          image_url={this.state.image_url}
+          description={this.state.description}
+        />
       </div>
     );
   }
