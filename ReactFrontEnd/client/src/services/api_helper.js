af503d040379037041ec520a765ae678684cdd78
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2020-01-17 10:04:43 -0500

    [Express] 🚁

--- ReactFrontEnd/client/src/services/api_helper.js
+++ ReactFrontEnd/client/src/services/api_helper.js
@@ -4,6 +4,8 @@ const api = axios.create({
   baseURL: "http://localhost:3001"
 });
 
+// AUTH API_HELPER API CALLS
+
 const api_helper = resp => {
   localStorage.setItem("authToken", resp.data.token);
   api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
@@ -12,13 +14,11 @@ const api_helper = resp => {
 
 export const loginUser = async loginData => {
   const resp = await api.post("/auth/login", loginData);
-  console.log(resp);
   return api_helper(resp);
 };
 
 export const registerUser = async registerData => {
   const resp = await api.post("/auth/register", registerData);
-  console.log(resp);
   return api_helper(resp);
 };
 
@@ -31,6 +31,8 @@ export const verifyUser = async () => {
   }
   return false;
 };
+
+// NEW POST API_HELPER API CALLS
 
 export const newPost = async newPost => {
   const resp = await api.post("/posts", newPost);
@@ -51,4 +53,25 @@ export const updatePost = async (id, updateData) => {
   const resp = await api.put(`/posts/${id}`, updateData);
   return resp.data;
 };
- 
+
+// NEW COMMENT API_HELPER API CALLS
+
+export const newPost = async newPost => {
+  const resp = await api.post("/comments", newPost);
+  return resp.data;
+};
+
+export const deletePost = async id => {
+  const resp = await api.delete(`/comments/${id}`);
+  return resp.data;
+};
+
+export const showPost = async id => {
+  const resp = await api.get(`/comments/${id}`);
+  return resp.data;
+};
+
+export const updatePost = async (id, updateData) => {
+  const resp = await api.put(`/comments/${id}`, updateData);
+  return resp.data;
+};
