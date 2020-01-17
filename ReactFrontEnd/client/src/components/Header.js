8d2fa760280d715862d1cd7a77a4f04c5c29ac84
rvy5009 <rvy5009@gmail.com>
2020-01-17 13:36:29 -0500

    [nav]

--- ReactFrontEnd/client/src/components/Header.js
+++ ReactFrontEnd/client/src/components/Header.js
@@ -3,12 +3,16 @@ import { Link } from "react-router-dom"
 
 const Header = (props) => {
   return (
-    <nav>
-      <Link to="/"> Home </Link>
-      <Link to="/createPost"> Create Post</Link>
-      <Link to="/profile"> Profile</Link>
-      <button onClick={props.handleLogout}>Logout</button>
-    </nav>
+    <div>
+
+      <nav>
+        <Link to="/"> Home </Link>
+        <Link to="/createPost"> Create Post</Link>
+        <Link to="/profile"> Profile</Link>
+        <button onClick={props.handleLogout}>Logout</button>
+      </nav>
+      
+      </div>
   )
 }
 
