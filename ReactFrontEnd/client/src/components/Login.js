dc9064ab0005fb8d5647d49c55849f29d8338587
Connor Lagana <connor.lagana@gmail.com>
2020-01-21 16:49:26 -0500

    fuck me up bro

--- ReactFrontEnd/client/src/components/Login.js
+++ ReactFrontEnd/client/src/components/Login.js
@@ -20,39 +20,44 @@ class Login extends Component {
   render() {
     return (
       <div id="login">
-        <div id="loginBackground">
-          <img src={foodBackground} alt="background" />
-        </div>
-        <div id="backframeLogin">
-          <form
-            onSubmit={e => {
-              this.props.handleLogin(e, {
-                username: this.state.username,
-                password: this.state.password
-              });
-            }}
-          >
-            <input
-              type="text"
-              name="username"
-              value={this.state.username}
-              onChange={this.onChange}
-              placeholder="Username5"
-              required
-            />
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.onChange}
-              placeholder="Password"
-              required
-            />
-            <input type="submit" value="Login" />
-            <Link to="/register">
-              <p className="signup">Not a member? Click to sign up</p>
-            </Link>
-          </form>
+        <div id="gradientBG">
+          <div id="backframeLogin">
+            <div id="leftLogin">
+              <img src="https://img.freepik.com/free-vector/modern-flat-website-login-page-template_2095-181.jpg?size=626&ext=jpg"></img>
+            </div>
+            <div id="rightLogin">
+              <h1>Member Login</h1>
+              <form
+                onSubmit={e => {
+                  this.props.handleLogin(e, {
+                    username: this.state.username,
+                    password: this.state.password
+                  });
+                }}
+              >
+                <input
+                  type="text"
+                  name="username"
+                  value={this.state.username}
+                  onChange={this.onChange}
+                  placeholder="Username5"
+                  required
+                />
+                <input
+                  type="password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  placeholder="Password"
+                  required
+                />
+                <button>Login</button>
+                <Link to="/register">
+                  <p className="signup">Not a member? Click to sign up</p>
+                </Link>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     );
