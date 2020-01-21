95281ca3ddabdf82a7f7fa077c379175a6f2dd90
rvy5009 <rvy5009@gmail.com>
2020-01-21 10:58:43 -0500

    need to commit to pull

--- ReactFrontEnd/client/src/components/Header.js
+++ ReactFrontEnd/client/src/components/Header.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-const Header = props => {
-  return (
-    <div className="headerRay">
-      <img
-        src="https://www.freeiconspng.com/uploads/instagram-photo-camera-logo-outline-icons--free-download-14.jpg"
-        alt="Foodstagram"
-        className="logoHeader"
-      />
-      <h1>FOODSTAGRAM</h1>
-      <nav>
+
+
+
+class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+    }
+  }
+
+myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+  render() {
+    return (
+      <div className="headerRay">
+        <img
+          src="https://www.freeiconspng.com/uploads/instagram-photo-camera-logo-outline-icons--free-download-14.jpg"
+          alt="Foodstagram"
+          className="logoHeader"
+        />
+        <h1>FOODSTAGRAM</h1>
+        <nav className="topnav" id="myTopnav" onClick= {this.myFunction} >
         <Link to="/home" className="navItem">
           Home
         </Link>
@@ -18,14 +39,15 @@ const Header = props => {
           Create Post
         </Link>
         <Link to="/profile" className="navItem">
-          {props.userName}
+          {this.props.userName}
         </Link>
-        <Link onClick={e => props.handleLogout(e)} className="navItem">
+        <Link onClick={e => this.props.handleLogout(e)} className="navItem">
           Logout
         </Link>
       </nav>
-    </div>
+    </div >
   );
+  }
 };
 
 export default Header;
