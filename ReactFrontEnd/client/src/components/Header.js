008eb537f3d3266e8ac32bd2ffb1b1391f57c99c
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2020-01-22 09:32:20 -0500

    Update

--- ReactFrontEnd/client/src/components/Header.js
+++ ReactFrontEnd/client/src/components/Header.js
@@ -27,7 +27,7 @@ class Header extends Component {
           alt="Foodstagram"
           className="logoHeader"
         />
-        <img src={fontLogo} id="#fontLogoHeader" />
+        <img src={fontLogo} id="fontLogoHeader" />
         <a href="#nowhere" className="icon" onClick={this.hamburger}>
           <i className="fa fa-bars"></i>
         </a>
